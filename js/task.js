b4e37cd1f6fa54b186844784525fd05a71a6a967
Aleruin <ale.xander.s@yandex.ru>
2019-05-20 16:01:55 +0700

    Незначительные изменения

--- js/task.js
+++ js/task.js
@@ -1,7 +1,8 @@
 window.onload = function() {
     const taskTableBody = document.getElementById('variant').getElementsByTagName('tbody')[0];
     const tdElements = taskTableBody.querySelectorAll('td');
-    console.log(tdElements);
+    //console.log(tdElements);
+    //console.log('variants.json');
 
     // $.getJSON("variants.json", function(json) {
     //     console.log(json); // this will show the info it in firebug console
@@ -9,4 +10,8 @@ window.onload = function() {
     // var jsonFiles = jsonGet();
     // tdElements[0].value = jsonFiles[0].rulesBase;
     // tdElements[1].value = jsonFiles[0].factsBase;
+}
+
+function ajaxJSON() {
+
 }
