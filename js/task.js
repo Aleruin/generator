6e629415f7e9fa12991da820facb9e33604ec5f3
Aleruin <ale.xander.s@yandex.ru>
2019-05-25 22:59:57 +0700

    В таблице с заданием отображаются пронумерованные правила из БП

--- js/task.js
+++ js/task.js
@@ -1,15 +1,23 @@
 window.onload = function() {
     const taskTableBody = document.getElementById('variant').getElementsByTagName('tbody')[0];
     const tdElements = taskTableBody.querySelectorAll('td');
+
     ajaxJSON("variants.json", function(text){
-        var data = JSON.parse(text);
-        tdElements[0].innerHTML = data[0].rulesBase;
+        let data = JSON.parse(text);
+
+        for (let i = 0; i < data[0].rulesBase.length; i++) {
+            let li = document.createElement('li');
+            li.innerHTML = data[0].rulesBase[i];
+            tdElements[0].querySelector('ol').appendChild(li);
+        }
+        
         tdElements[1].innerHTML = data[1].factsBase;
     });
  }
 
 function ajaxJSON(file, callback) {
     var request = new XMLHttpRequest();
+
     request.overrideMimeType("application/json");
     request.open("GET", file, true);
     request.onreadystatechange = function() {
@@ -17,6 +25,7 @@ function ajaxJSON(file, callback) {
             callback(request.responseText);
         }
     }
+
     request.send(null); 
 
 }
--- server.js
+++ server.js
@@ -6,7 +6,7 @@ http.createServer(function(req, res) {
   file.serve(req, res);
 }).listen(8080);
 
-console.log('Server running on port 8080');
+console.log('Connection established');
 
 
 
